7e7218848ca750bc39ab81efeecc27dce154f8d9
Yu MORIYA <euro21st@gmail.com>
2014-02-23 12:33:21 +0900

    説明書ロード処理追加

--- twister.user.js
+++ twister.user.js
@@ -4,31 +4,26 @@
 // @version     0.1
 // @description ねじれ天国向け便利ツール群です。
 // @match       http://nejiten.wkeya.com/index.cgi*
-// @require     http://code.jquery.com/jquery-1.8.2.js
+// @require     http://code.jquery.com/jquery-2.1.0.min.js
 // @copyright   2014+ Yu MORIYA
 // ==/UserScript==
 
-(function() {
-
+(function main($) {
+	var uri = GM_info.script.matches[0].replace('*', '');
 	var docs = GM_getValue('docs');
 	if (docs == null) {
-		
+		var docsUri = uri + '?cmd=doc';
+		alert('loading...');
+		$.get(docsUri)
+			.done(function(data) {
+				alert('done.')
+				docs = data;
+				GM_setValue('docs', docs);
+				main($);
+			});
+	} else {
+		alert('loaded.');
+		//GM_deleteValue('docs');
 	}
 
-    function GM_getValue(key , defaultValue)
-    {
-      var value = window.localStorage.getItem(key);
-      if (value != null) {
-        return eval(value);
-      } else {
-        return defaultValue || null;
-      }
-    }
-
-    function GM_setValue(key , value)
-    {
-      window.localStorage.setItem(key , value);
-    }
-
-
-})(); 
+})(jQuery); 
