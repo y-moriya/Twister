0d4d09d492811d4d140945944ba5fee363d97319
Yu MORIYA <euro21st@gmail.com>
2014-02-23 15:10:34 +0900

    説明書ポップアップは一通り実装

--- twister.user.js
+++ twister.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        Twister
 // @namespace   http://www.gunjobiyori.com/
-// @version     0.1
+// @version     0.2
 // @description ねじれ天国向け便利ツール群です。
 // @match       http://nejiten.wkeya.com/index.cgi*
 // @require     http://code.jquery.com/jquery-2.1.0.min.js
+// @require     http://raw.github.com/urin/jquery.balloon.js/master/jquery.balloon.min.js
 // @copyright   2014+ Yu MORIYA
 // ==/UserScript==
 
@@ -13,8 +14,37 @@
 	
 	var raw = GM_getValue('docs_raw');
 	if (raw == null) {
+		loadDocs(uri);
+		parseDocs(raw);
+	} else {
+		if (location.href.match('vid')) {
+			showVersion();
+			setDescription();
+		}
+	}
+	
+	function setDescription() {
+		$('a')
+			.filter(function() { if (this.href.match('cmd=doc')) { return this }})
+			.each(function() {
+				var sNum = 0;
+				var content = '役職が見つかりませんでした。';
+				if (this.href.match(/cmd=doc\#([0-9]+)/)) {
+					content = GM_getValue('docs_' + RegExp.$1);
+				}
+				$(this).balloon({
+					contents: content,
+					position: "bottom right",
+					css: {
+						maxWidth: '520px'
+					}
+				});
+			});
+	}
+	
+	function loadDocs(uri) {
 		var docsUri = uri + '?cmd=doc';
-		alert('説明書をロードします。しばらくお待ちください。');
+		alert('説明書をロードします。しばらくお待ちください。（初回のみ）');
 		$.get(docsUri)
 			.done(function(data) {
 				raw = data;
@@ -23,31 +53,27 @@
 				twister($);
 				return;
 			});
-	} else {
-		showVersion();
-		parseDocs(raw);
-		var s = GM_getValue('docs');
-		s.forEach(function(e) {
-			//alert(e);
-		});
 	}
 	
 	function showVersion() {
 		var sName = GM_info.script.name;
 		var version = GM_info.script.version;
-		var verHtml = $('<div>using ' + sName + ' ver. ' + version + '</div>')
+		var reloadHtml = $('<div><a href="#">cache reload</a></div>')
 						.css('text-align', 'right')
 						.click(function() {
 				 			alert('説明書を再取得します。');
 				 			GM_deleteValue('docs_raw');
-				 			$(this).remove();
+				 			$(this).parent().remove();
 				 			twister($);
 				 		});
+		var verHtml = $('<div><div>using ' + sName + ' ver. ' + version + '</div></div>')
+						.css('text-align', 'right')
+						.append(reloadHtml);
 		$('body').prepend(verHtml);
 	}
 	
 	function parseDocs(raw) {
-		var skills = [];
+		var i = 0;
 		$(raw)
 			.find('a.large_doc')
 			.filter(function() { return this.name.match(/^[0-9]+$/);})
@@ -57,11 +83,11 @@
 				var p = obj.parent();
 				p.find('a').remove();
 				p = $(p.html().replace(/<table(\n|.)+/, ''));
-				var d = $.trim(p.text());
-				var html = "<div><h3>" + n + "</h3><p>" + d + "</p></div>";
-				skills.push(html);
+				var d = $.trim(p.text()).replace(/\n/g, "<br>");
+				var html = "<div><h3>" + n + "</h3><p style=\"font-size: small;\">" + d + "</p></div>";
+				GM_setValue('docs_'+i, html);
+				i++;
 			});
-		GM_setValue('docs', skills);
 	}
 
 })(jQuery); 
