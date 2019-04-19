3210897922312ecdde82fb18a7701e657199a0b8
Yu <euro21st@gmail.com>
2019-04-19 15:23:29 +0900

    文章の開閉ができるようになった

--- literature.user.js
+++ literature.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name        Literature
+// @name        Block Literature
 // @namespace   http://www.gunjobiyori.com/
-// @version     0.1.0
+// @version     1.0.0
 // @description ねじれ天国の文学対策スクリプトです。
-// @match       http://nejiten.halfmoon.jp/index.cgi*
+// @match       http://nejiten.halfmoon.jp/*
 // @copyright   2019 Yu MORIYA
 // ==/UserScript==
 
@@ -18,6 +18,7 @@
 		let a = document.createElement('a')
 		a.id = 'open' + mes_num;
 		a.innerText = OPEN_MES;
+		a.style.cursor = 'pointer';
 		div.append(a);
 		return div;
 	}
@@ -28,6 +29,7 @@
 		let a = document.createElement('a')
 		a.id = 'close' + mes_num;
 		a.innerText = CLOSE_MES;
+		a.style.cursor = 'pointer';
 		div.append(a);
 		return div;
 	}
@@ -44,7 +46,7 @@
 	for (let i = 0; i < mes.length; i++) {
 		if (MAX_LENGTH < mes[i].innerText.length) {
 			let msg = mes[i].innerText;
-			let shortmsg = msg.slice(0, MAX_LENGTH) + '...';
+			let shortmsg = msg.slice(0, MAX_LENGTH) + "...\n\n";
 			mes[i].innerText = '';
 
 			// 長文を退避
@@ -58,10 +60,35 @@
 			let openDiv = createOpenDiv(i);
 			shortmsgdiv.append(openDiv);
 
+			// クリック時に開閉を呼ぶようにイベントリスナ設定
+			closeDiv.addEventListener('click', function() {
+				toggle(i, true);
+			});
+			openDiv.addEventListener('click', function() {
+				toggle(i, false);
+			});
+
 			// 追記
 			mes[i].append(shortmsgdiv);
 			mes[i].append(msgdiv);
 
+		}
+	}
+
+	function toggle(i, isOpenend) {
+		const msgdivid = 'msg' + i;
+		const shortmsgdivid = 'shortmsg' + i;
+		let msgdiv = document.getElementById(msgdivid);
+		let shortmsgdiv = document.getElementById(shortmsgdivid);
+
+		if (isOpenend) {
+			// 全文表示から短縮表示に切り替える
+			msgdiv.style.display = 'none';
+			shortmsgdiv.style.display = 'inline';
+		} else {
+			// 短縮表示から全文表示に切り替える
+			shortmsgdiv.style.display = 'none';
+			msgdiv.style.display = 'inline';
 		}
 	}
 })();
